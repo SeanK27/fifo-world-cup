97083137e1aeaba6fb1dd31a264f452822c76d9c
Nora Su <nosu5119@colorado.edu>
2026-03-07 16:28:09 -0700

    high level multiplayer stuff

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,8 +4,13 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+func _enter_tree() -> void:
+	set_multiplayer_authority(name.to_int())
+
 
 func _physics_process(delta: float) -> void:
+	
+	if not is_multiplayer_authority(): return
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -23,3 +28,5 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	
