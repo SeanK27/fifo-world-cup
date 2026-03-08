2300f3b134010a7c9ecdd29e20424b605492c709
Nora Su <nosu5119@colorado.edu>
2026-03-07 20:54:21 -0700

    it not work

--- scenes/betting.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func _ready() -> void:
-	$MenuButton.pressed.connect(_on_menu_pressed)
-
-func _on_menu_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
