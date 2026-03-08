1a2e2e3b9aa9f6b1995d4fe09771abd28c1057b8
SeanK27 <seankk100@gmail.com>
2026-03-07 19:06:14 -0700

    Main menu + making gambling menu nicer

---
+++ scenes/main_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _ready() -> void:
+	$VBoxContainer/Button3.pressed.connect(_on_shoppe_pressed)
+	$VBoxContainer/Button4.pressed.connect(_on_quit_pressed)
+
+func _on_shoppe_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/betting.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
