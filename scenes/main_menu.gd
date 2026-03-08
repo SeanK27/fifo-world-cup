dafd70e8c75529629e6bbfa668bf73b9166645a3
SeanK27 <seankk100@gmail.com>
2026-03-07 19:20:10 -0700

    Play button activated

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -3,6 +3,10 @@ extends Control
 func _ready() -> void:
 	$VBoxContainer/Button3.pressed.connect(_on_shoppe_pressed)
 	$VBoxContainer/Button4.pressed.connect(_on_quit_pressed)
+	$VBoxContainer/Button2.pressed.connect(_on_play_pressed)
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/soccer.tscn")
 
 func _on_shoppe_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/betting.tscn")
