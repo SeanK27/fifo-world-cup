c49e7b94e5e5df2b28f40d17f594d0b7128deeb4
SeanK27 <seankk100@gmail.com>
2026-03-07 20:35:12 -0700

    Ball resets to center when goal :)

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -10,8 +10,22 @@ extends RigidBody2D
 #func _process(delta):
 	#pass
 
-const FRICTION = 0.9 
+const FRICTION = 0.9
 
-func _physics_process(delta: float) -> void:
+var teleport_requested := false
+var teleport_position := Vector2.ZERO
+
+func teleport(pos: Vector2) -> void:
+	teleport_position = pos
+	teleport_requested = true
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	if teleport_requested:
+		state.transform = Transform2D(0, teleport_position)
+		state.linear_velocity = Vector2.ZERO
+		state.angular_velocity = 0.0
+		teleport_requested = false
+
+func _physics_process(_delta: float) -> void:
 	linear_velocity *= FRICTION
 	gravity_scale = 0
