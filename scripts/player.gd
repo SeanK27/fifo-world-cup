49752e1d9832dababe3428dd7c22c1dd52dadebf
Nora Su <nosu5119@colorado.edu>
2026-03-07 20:54:48 -0700

    ah

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,10 +2,14 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
-func _enter_tree() -> void:
+func _ready() -> void:
 	set_multiplayer_authority(name.to_int())
+	var sync = $MultiplayerSynchronizer
+	sync.set_multiplayer_authority(name.to_int())
+
+#func _enter_tree() -> void:
+	#set_multiplayer_authority(name.to_int())
 
 
 # handle collision with ball objects
@@ -21,21 +25,10 @@ func _on_kick_area_body_entered(body):
 func _physics_process(delta: float) -> void:
 	
 	if not is_multiplayer_authority(): return
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	
 	var input_vector =  Vector2.ZERO
 	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	
-	#var direction := Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	velocity = input_vector.normalized() * SPEED
 	
